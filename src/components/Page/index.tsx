83f67364d5cdabce997f589d10f7d0d5b8b93027
lucasellery <lucas.ellery@somapay.com.br>
2020-11-25 15:03:56 -0300

    More styles

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from './styles';
+import { ModelSection, ModelsWrapper } from "../Model";
+import { Container } from "./styles";
+import DefaultOverlayContent from "../DefaultOverlayContent";
 
 const Page: React.FC = () => {
   return (
     <Container>
-      <h1>Page</h1>
+      <ModelsWrapper>
+        <div>
+          {[
+            "Model One",
+            "Model Two",
+            "Model Three",
+            "Model Four",
+            "Model Five",
+            "Model Six",
+            "Model Seven",
+          ].map((modelName) => (
+            <ModelSection
+              key={modelName}
+              className="colored"
+              modelName={modelName}
+              overlayNode={
+                <DefaultOverlayContent
+                  label={modelName}
+                  description="Order Online for Delivery"
+                />
+              }
+            />
+          ))}
+        </div>
+      </ModelsWrapper>
     </Container>
   );
-}
+};
 
 export default Page;
